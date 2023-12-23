ea2a79944455659f63d7a61a139aad31ef82c7d8
FL45h-09 <vishal.ravanank@gmail.com>
2023-12-23 16:43:14 +0530

    product card image link added

--- src/components/global/ProductCard.jsx
+++ src/components/global/ProductCard.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 export const ProductCard = (props) => {
   return (
     <div className='productcardsec'>
-        <div className="imgbox"><img src={props.imgsrc} alt={props.title} /></div>
+        <div className="imgbox"><NavLink to={`/products/${props.sku}`} ><img src={props.imgsrc} alt={props.title} /></NavLink>  </div>
         <div className="contentbox">
             <h3><NavLink to={`/products/${props.sku}`} >{props.title}</NavLink></h3>
             <h5>Product Sku: {props.sku}</h5><p>Priced from ${props.price}</p>
